4c0ded96e85f6ccfda9a77091e0db717c733387e
David Inoa <davidinoa@gmail.com>
2019-07-20 23:57:07 -0700

    implement helper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Palette {...seedColors[4]} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Palette.js
+++ src/Palette.js
@@ -3,14 +3,15 @@ import ColorBox from './ColorBox';
 import './Palette.css';
 
 class Palette extends Component {
-    render() {
-        const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
-        return (
-            <div className="Palette">
-                <div className="Palette-colors">{colorBoxes}</div>
-            </div>
-        );
-    }
+  render() {
+    const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
+
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">{colorBoxes}</div>
+      </div>
+    );
+  }
 }
 
 export default Palette;
